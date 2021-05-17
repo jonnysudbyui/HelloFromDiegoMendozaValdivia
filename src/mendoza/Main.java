4fa74118b3925854cc1c237331fbceb3d2827dd2
jonnysudbyui <83973989+jonnysudbyui@users.noreply.github.com>
2021-05-16 23:27:48 -0500

    Add my name

--- src/mendoza/Main.java
+++ src/mendoza/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello My name is Diego Mendoza");
+        System.out.println("Hello Diego. My name is Jonathan") 
     }
 }
